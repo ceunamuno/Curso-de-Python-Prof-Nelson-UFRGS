7f16365427474937ed2725e2c4ac47c7b8cc31d7
ceunamuno <116899026+ceunamuno@users.noreply.github.com>
2022-10-28 11:54:01 -0300

    Add files via upload

---
+++ Entrada de dados por atribuição de variáveis.py
@@ -0,0 +1,10 @@
+#Programa: Faça um programa que entre os números 2 e 3 na memória do computador.
+#Autor: César Augusto Del Lama de Unamuno
+#Data: 28-10-2022
+
+#Entrada de dados
+a = int(input(' Entre com o primeiro número:  '))
+b = int(input(' Entre com o segundo número: '))
+
+#Saída de dados
+print(f'Os números digitados foram {a} e {b}.')
